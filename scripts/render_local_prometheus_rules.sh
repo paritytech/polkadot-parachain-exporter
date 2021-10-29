f359d5ad5d2375bb3f32d9a14c6ead410fbfbb31
Haiko Schol <haiko@parity.io>
2021-10-29 11:01:51 +0200

    Use render_local_prometheus_rules.sh in "test" job

--- scripts/render_local_prometheus_rules.sh
+++ scripts/render_local_prometheus_rules.sh
@@ -5,8 +5,9 @@ set -a
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROJECT_DIR="${SCRIPT_DIR}/.."
 PROMETHEUS_DIR="${PROJECT_DIR}/prometheus"
+ENV_FILE="${PROJECT_DIR}/.env"
 
-source "${PROJECT_DIR}/.env"
+[[ -f $ENV_FILE ]] && source $ENV_FILE
 export labels="\$labels"
 
 cat "${PROMETHEUS_DIR}/rules.yml" | envsubst > "${PROMETHEUS_DIR}/rules.local.yml"
